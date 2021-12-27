6caa6e6ff7e37f45b3a3bb4534fedb34f75a97eb
AbubakrRehman <abubakr.rehman91@outlook.com>
2021-12-27 23:34:45 +0530

    db chaned

--- server/controllers/mysqlConnection.js
+++ server/controllers/mysqlConnection.js
@@ -1,20 +1,20 @@
 const mysql2 = require('mysql2');
 
-// var connection = mysql2.createConnection({
-//     host     : 'db-mysql-blr1-49817-do-user-10215911-0.b.db.ondigitalocean.com',
-//     user     : 'doadmin',
-//     password : 'GtCoH30d4BBgng7g',
-//     database : 'defaultdb',
-//     port:25060
-//   });
-
-  var connection = mysql2.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'Abubakr@7086',
-    database : 'my_db',
-    port:3306
+var connection = mysql2.createConnection({
+    host     : 'db-mysql-blr1-77276-do-user-10515190-0.b.db.ondigitalocean.com',
+    user     : 'doadmin',
+    password : 'IzAuJnoqnKcC7cRV',
+    database : 'defaultdb',
+    port:25060
   });
+
+  // var connection = mysql2.createConnection({
+  //   host     : 'localhost',
+  //   user     : 'root',
+  //   password : 'Abubakr@7086',
+  //   database : 'my_db',
+  //   port:3306
+  // });
 
   connection.connect(function(err){
     if (err) {
